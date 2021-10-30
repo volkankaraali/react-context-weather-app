bb559ee1a33f02c77322d4f41bca40d9cc59747a
volkan karaali <volkankaraali1@gmail.com>
2021-10-30 19:17:20 +0300

    api url fix

--- src/services/weatherService.jsx
+++ src/services/weatherService.jsx
@@ -5,7 +5,7 @@ export const apiKey="9add691b6641cfa2a60059fc1f132c8f";
 export default class WeatherService{
 
     getCityLatAndLon(cityName){
-        let apiurl=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
+        let apiurl=`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
         return axios.get(apiurl)
     }
 
