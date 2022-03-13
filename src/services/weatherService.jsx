1a4de7a06aa49eb614f33fca61b518de0e183fc4
volkan karaali <volkankaraali1@gmail.com>
2022-03-13 16:36:30 +0300

    refactoring 2

--- src/services/weatherService.jsx
+++ src/services/weatherService.jsx
@@ -1,16 +1,16 @@
 import axios from "axios"
 
 
-export const apiKey="9add691b6641cfa2a60059fc1f132c8f";
-export default class WeatherService{
+const apiKey = "9add691b6641cfa2a60059fc1f132c8f";
+export default class WeatherService {
 
-    getCityLatAndLon(cityName){
-        let apiurl=`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
+    getCityLatAndLon(cityName) {
+        let apiurl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
         return axios.get(apiurl)
     }
 
-    getWeeklyWeatherOfCityByLatAndLon(lat,lon){
-        let apiurl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+    getWeeklyWeatherOfCityByLatAndLon(lat, lon) {
+        let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
         return axios.get(apiurl)
     }
 }
